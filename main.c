eaee1e669adb3a4fd465ececa160a7de18588300
vehar <vladdxmel@gmail.com>
2017-08-13 14:59:37 +0300

    add UART+DMA

--- main.c
+++ main.c
@@ -5,8 +5,9 @@
 #include "stm32f10x_adc.h"
 #include "stm32f10x_dma.h"
 #include "stm32f10x_tim.h"
-//#include "stm32f10x_spi.h"
+
 #include "stdio.h"
+#include <string.h>
 #include "misc.h"
 
 //------
@@ -14,6 +15,143 @@
 #include "usb_lib.h"
 #include "usb_desc.h"
 #include "usb_pwr.h"
+
+
+#define RX_BUF_SIZE 80
+volatile char RX_FLAG_END_LINE = 0;
+volatile char RXi;
+volatile char RXc;
+ char RX_BUF[RX_BUF_SIZE] = {'\0'};
+ char buffer[80] = {'\0'};
+
+void clear_RXBuffer(void) {
+	for (RXi=0; RXi<RX_BUF_SIZE; RXi++)
+		RX_BUF[RXi] = '\0';
+	RXi = 0;
+}
+
+void usart_dma_init(void)
+{
+	/* Enable USART1 and GPIOA clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
+
+	/* DMA */
+	DMA_InitTypeDef DMA_InitStruct;
+	DMA_InitStruct.DMA_PeripheralBaseAddr = (uint32_t)&(USART1->DR);
+	DMA_InitStruct.DMA_MemoryBaseAddr = (uint32_t)&buffer[0];
+	DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralDST;
+	DMA_InitStruct.DMA_BufferSize = sizeof(buffer);
+	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+	DMA_InitStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+	DMA_InitStruct.DMA_Mode = DMA_Mode_Normal;
+	DMA_InitStruct.DMA_Priority = DMA_Priority_Low;
+	DMA_InitStruct.DMA_M2M = DMA_M2M_Disable;
+	DMA_Init(DMA1_Channel4, &DMA_InitStruct);
+
+	/* NVIC Configuration */
+	NVIC_InitTypeDef NVIC_InitStructure;
+	/* Enable the USARTx Interrupt */
+	NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+	/* Configure the GPIOs */
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	/* Configure USART1 Tx (PA.09) as alternate function push-pull */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure USART1 Rx (PA.10) as input floating */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Configure the USART1 */
+	USART_InitTypeDef USART_InitStructure;
+
+	/* USART1 configuration ------------------------------------------------------*/
+	/* USART1 configured as follow:
+		- BaudRate = 115200 baud
+		- Word Length = 8 Bits
+		- One Stop Bit
+		- No parity
+		- Hardware flow control disabled (RTS and CTS signals)
+		- Receive and transmit enabled
+		- USART Clock disabled
+		- USART CPOL: Clock is active low
+		- USART CPHA: Data is captured on the middle
+		- USART LastBit: The clock pulse of the last data bit is not output to
+			the SCLK pin
+	 */
+	USART_InitStructure.USART_BaudRate = 115200;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+	USART_Init(USART1, &USART_InitStructure);
+
+	/* Enable USART1 */
+	USART_Cmd(USART1, ENABLE);
+
+	USART_DMACmd(USART1, USART_DMAReq_Tx, ENABLE);
+	//DMA_Cmd(DMA1_Channel4, ENABLE);
+
+	DMA_ITConfig(DMA1_Channel4, DMA_IT_TC, ENABLE);
+	NVIC_EnableIRQ(DMA1_Channel4_IRQn);
+
+
+	/* Enable the USART1 Receive interrupt: this interrupt is generated when the
+	USART1 receive data register is not empty */
+	USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+}
+
+void USART1_IRQHandler(void)
+{
+    if ((USART1->SR & USART_FLAG_RXNE) != (u16)RESET)
+	{
+    		RXc = USART_ReceiveData(USART1);
+    		RX_BUF[RXi] = RXc;
+    		RXi++;
+
+    		if (RXc != 13) {
+    			if (RXi > RX_BUF_SIZE-1) {
+    				clear_RXBuffer();
+    			}
+    		}
+    		else {
+    			RX_FLAG_END_LINE = 1;
+    		}
+
+			//Echo
+    		USART_SendData(USART1, RXc);
+	}
+}
+
+void USARTSendDMA(char *pucBuffer)
+{
+	strcpy(buffer, pucBuffer);
+
+	/* Restart DMA Channel*/
+	DMA_Cmd(DMA1_Channel4, DISABLE);
+	DMA1_Channel4->CNDTR = strlen(pucBuffer);
+	DMA_Cmd(DMA1_Channel4, ENABLE);
+}
+
+void DMA1_Channel4_IRQHandler(void)
+{
+	DMA_ClearITPendingBit(DMA1_IT_TC4);
+	DMA_Cmd(DMA1_Channel4, DISABLE);
+}
 
 volatile short FLAG_ECHO = 0;
 
@@ -213,6 +351,31 @@ void ADC_DMA_init(void)
 	while(ADC_GetCalibrationStatus(ADC1));
 	ADC_SoftwareStartConvCmd ( ADC1 , ENABLE ) ;
 }
+
+void Timer4_init(void)
+{
+	 // TIMER4
+    TIM_TimeBaseInitTypeDef TIMER_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+  	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+
+  	TIM_TimeBaseStructInit(&TIMER_InitStructure);
+    TIMER_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
+    TIMER_InitStructure.TIM_Prescaler = 7200;
+    TIMER_InitStructure.TIM_Period = 5000;
+    TIM_TimeBaseInit(TIM4, &TIMER_InitStructure);
+    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM4, ENABLE);
+
+    /* NVIC Configuration */
+    /* Enable the TIM4_IRQn Interrupt */
+    NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStructure);
+}
 //=================================================================================
 	//----------------
 	extern volatile uint8_t Receive_Buffer[64];
@@ -239,30 +402,57 @@ int main(void)
 	SetSysClockTo72();
 
 
-    usart_init(); //USART
+    //usart_init(); //USART
+	//*
+	
+		// Initialize LED which connected to PC13  
+	GPIO_InitTypeDef  GPIO_InitStructure;
+	// Enable PORTC Clock
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+	// Configure the GPIO_LED pin  
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+
+	GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Set C13 to Low level ("0")
+
+    // Initialize USART
+    usart_dma_init();
+    USARTSendDMA("Hello.\r\nUSART1 is ready.\r\n");
+	
+	 while (1)
+    {
+    	if (RX_FLAG_END_LINE == 1) 
+				{
+    		// Reset END_LINE Flag
+    		RX_FLAG_END_LINE = 0;
+
+    		/* !!! This lines is not have effect. Just a last command USARTSendDMA(":\r\n"); !!!! */
+    		USARTSendDMA("\r\nI has received a line:\r\n"); // no effect
+    		USARTSendDMA(RX_BUF); // no effect
+    		USARTSendDMA(":\r\n"); // This command does not wait for the finish of the sending of buffer. It just write to buffer new information and restart sending via DMA.
+
+    		if (strncmp(RX_BUF, "ON\r", 3) == 0) 
+				{
+    			USARTSendDMA("THIS IS A COMMAND \"ON\"!!!\r\n");
+    			GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+    		}
+
+    		if (strncmp(RX_BUF, "OFF\r", 4) == 0) 
+				{
+    			USARTSendDMA("THIS IS A COMMAND \"OFF\"!!!\r\n");
+    			GPIO_SetBits(GPIOC, GPIO_Pin_13);
+    		}
+
+    		clear_RXBuffer();
+    	}
+    }
+		
+	//*/
     ADC_DMA_init();//ADC
 
-    // TIMER4
-    TIM_TimeBaseInitTypeDef TIMER_InitStructure;
-    NVIC_InitTypeDef NVIC_InitStructure;
-
-  	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
-
-  	TIM_TimeBaseStructInit(&TIMER_InitStructure);
-    TIMER_InitStructure.TIM_CounterMode = TIM_CounterMode_Up;
-    TIMER_InitStructure.TIM_Prescaler = 7200;
-    TIMER_InitStructure.TIM_Period = 5000;
-    TIM_TimeBaseInit(TIM4, &TIMER_InitStructure);
-    TIM_ITConfig(TIM4, TIM_IT_Update, ENABLE);
-    TIM_Cmd(TIM4, ENABLE);
-
-    /* NVIC Configuration */
-    /* Enable the TIM4_IRQn Interrupt */
-    NVIC_InitStructure.NVIC_IRQChannel = TIM4_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
+   //Timer4_init();
 
 
 	Set_USBClock(); //---
